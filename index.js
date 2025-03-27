51e06e5b54e2e04d8f25c4aa2f155080a4f84ee8
srimanta-g <srimantaganguly02@gmail.com>
2025-03-28 01:09:20 +0530

    add cli commands and set up express server

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello world");
+const yargs = require("yargs");
+const express = require("express");
+
+const expressApp = express();
+// const yargs = require;
+
+yargs
+	.command({
+		command: "caching-proxy",
+		describe: "To start the caching server",
+		builder: {
+			port: {
+				describe: "Port at which the caching server will listen to.",
+				demandOption: true,
+				type: Number,
+			},
+			url: {
+				describe: "Traget url",
+				demandOption: true,
+				type: String,
+			},
+		},
+		handler(argv) {
+			// console.log(argv.port + " " + argv.url);
+			startServer(argv.port);
+		},
+	})
+	.help().argv;
+
+function startServer(port) {
+	expressApp.listen(port, () => {
+		console.log("Server started at port" + port);
+	});
+}
